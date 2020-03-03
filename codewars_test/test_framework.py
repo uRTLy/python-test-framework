692162ba6442b05e7a39f2c56481819ae700baf6
GMI <gmi@Patricks-MacBook-Pro.local>
2020-03-03 17:38:09 +0100

    added colors and spacer around test results

--- codewars_test/test_framework.py
+++ codewars_test/test_framework.py
@@ -1,6 +1,17 @@
 from __future__ import print_function
 
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
 class AssertException(Exception):
     pass
 
@@ -10,7 +21,9 @@ def format_message(message):
 
 
 def display(type, message, label="", mode=""):
-    print("\n<{0}:{1}:{2}>{3}".format(
+    color = bcolors.WARNING if type == 'FAILED' else bcolors.OKGREEN
+    spacer = '\n-------------------------------\n'
+    print((color + spacer + "<{0}:{1}:{2}>{3}" + spacer).format(
         type.upper(), mode.upper(), label, format_message(message)))
 
 
